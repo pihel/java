cf61f9461ee033767f9118343725fbbaedab2850
pihel <skahinpihel@gmail.com>
2016-10-05 20:54:09 +0300

    binary search

--- oracle_sort/OracleSort.java
+++ oracle_sort/OracleSort.java
@@ -255,6 +255,19 @@ public class OracleSort {
 		}
 	}
 	
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+	
 	public static void radixSortUInt(Integer[] arr) {
 		final int RADIX = 10;
 		  // declare and initialize bucket[]
