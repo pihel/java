9f17bc5d8c976f07bffa648c20309913923a4d18
pihel <skahinpihel@gmail.com>
2016-10-15 21:53:12 +0300

    comments

--- oracle_sort/OracleSort.java
+++ oracle_sort/OracleSort.java
@@ -2,19 +2,25 @@ package oracle_sort;
 
 import java.util.*;
 
+/*
+ * Односвязный список
+ * */
 class Node {
 	Integer val;
 	Node next;
 	Node root;
  
+	//создание элемента списка
 	Node(Integer x) {
 		val = x;
 		next = null;
 	}
 	
+	//вставка массива в список
 	public Node(Integer[] arr) {
 		root = null;
 		Node prev = null;
+		//идем с конца массива - так проще, т.к. у нас только указатель на следующий
 		for(int i = arr.length - 1; i >= 0; i--) {
 		    //последний элемент с конца - голова списка
 			root = new Node(arr[i]);
@@ -36,6 +42,7 @@ class Node {
 		}*/
 	} //Node
 	
+	//печать списка
 	public void print() {
 		root = this;
 		while(root != null) {
@@ -45,53 +52,12 @@ class Node {
 	} //print
 }
 
-class InsertSortList {
-	
-	public static Node sort(Node root) {
-		root = root.root;
-		if(root == null) return root;
+/*
+Бинарное дерево поиска:
+	* Сложность: N*Log2(N)
+	* Доп. Память: O(N)
 		
-		//новый отсортированный список
-		Node sort_node = new Node(root.val);
-		
-		Node pointer = root.next;
-		
-		//идем вперед
-		while(pointer != null) {
-			Node inner_node = sort_node;
-			Node next = pointer.next;
-			
-			//если элемент меньше начала, то это наше новое начало
-			if(pointer.val <= sort_node.val) {
-				Node swp = sort_node;
-				sort_node = pointer;
-				sort_node.next = swp;
-			} else {
-				//иначе идем с начала списка еще раз
-				while(inner_node.next != null) {
-					//находим место вставки
-					if (pointer.val > inner_node.val && pointer.val <= inner_node.next.val) {
-						Node oldNext = inner_node.next;
-						inner_node.next = pointer;
-						pointer.next = oldNext;
-					}					
-					inner_node = inner_node.next;
-				}
-				
-				//дошли доконца - это наш новый конец
-				if (inner_node.next == null && pointer.val > inner_node.val) {
-					inner_node.next = pointer;
-					pointer.next = null;
-				}
-			}
-			
-			pointer = next;
-		}
-		
-		return sort_node;
-	} //sort
-} //InsertSortList
-
+*/
 class Tree<T extends Comparable<T>> {
 	public T val;
 	public Tree<T> left;
@@ -101,89 +67,129 @@ class Tree<T extends Comparable<T>> {
 		val = x;
 	}
 	
+	//вставка массива в список
    Tree( T[] arr) {
 	   for(int i = 0; i < arr.length; i++) {
 		   this.insert(new Tree<T>(arr[i]));
 	   }
    }
 	
+   //вставка поддерева в дерево
    public void insert( Tree<T> aTree) {
 
+	 //если корня нет - это наш новый корень
 	 if(val == null) {
 		 val = aTree.val;
 		 return;
 	 }
-		 
+	
+	 //если вставляемый элемент меньше текущего
 	 if (aTree.val.compareTo(val) < 0 ) {
+		//идем налево дерева
 	    if ( left != null ) {
+	    	//если в левой части есть дерево, то рекурсивно спускаемся
 	    	left.insert( aTree );
 	    } else {
+	    	//иначе создаем новый лист
 	    	left = aTree;
 	    }
 	 } else {
+		//если больше, то движемся направо
 	    if ( right != null ) {
+	    	//если в правой части есть дерево, то спускаемся
 	    	right.insert( aTree );
 	    }
 	    else { 
+	    	//иначе создаем лист
 	    	right = aTree;
 	    }
 	 }
    }
    
+   //обход дерева
    public void traverse() {
 	   if(left != null) {
+		   //спускаемся налево, пока не дойдем до листа
 		   left.traverse();
 	   }
+	   //доидя до листа раскручиваем рекурсию обратно
 	   System.out.print(val + ", ");
 	   
+	   //пока не найдем поворот направо
 	   if(right != null) {
+		   //спускаемся на 1 элемента вправо
 		   right.traverse();
 	   }
    }
 } //Tree
 
+
+/*
+Сортировка слиянием: 
+	* Сложность: N*Log2(N)
+	* Доп. память: O(N)
+	* Общий смысл:
+		** рекурсивно разбиваем масиив на 2 части, пока размер элемента не дойдет до N (тут =1 )
+		** сливаем массивы в один последовательным проходом (в худшем случае N+M), сравнивая элементы
+*/
 class MergeSort {
 	
+	//проверка достаточности разбиения
 	@SuppressWarnings("rawtypes") 
 	protected boolean isSortArr(Comparable[] arr) {
 		if(arr.length <= 1) return true;
 		return false;
 	}
 	
+	//разбитие массива на 2 равные части
 	@SuppressWarnings("rawtypes") 
 	public void sort(Comparable[] arr) {
+		
+		//если массив достаточно разбит, то прерываем рекурсию
 		if(isSortArr(arr)) return;
 		
+		//левая половина
 		Comparable[] left = new Comparable[arr.length / 2];		
 	    System.arraycopy(arr, 0, left, 0, left.length);
 	    sort(left);
 	    //swop to disk
 	    
-	    
+	    //правая половина
 	    Comparable[] right = new Comparable[arr.length - left.length];
 	    System.arraycopy(arr, left.length, right, 0, right.length);
 	    sort(right);
 	     
+	    //слияние
 	    merge(left, right, arr);
 	} //sort
 	
+	
+	//слияние 2 упорядоченных массивов в один
 	@SuppressWarnings({ "rawtypes", "unchecked" }) 
 	private void merge(Comparable[] left, Comparable[] right, Comparable[] arr) {
 		int iFirst = 0;        
 	    int iSecond = 0;         
 	    int iMerged = 0;
 	     
+	    //бежим, пока не дойдем до конца одного из массивов
 	    while (iFirst < left.length && iSecond < right.length) {
+	    	//если элемент в левом массиве больше, чем в правом
 	        if (left[iFirst].compareTo(right[iSecond]) < 0) {
+	        	//то добавляем элемент из левого
 	        	arr[iMerged] = left[iFirst];
+	        	//и двигаемся в левом на 1
 	            iFirst++;
 	        } else {
+	        	//иначе добавляем элемент из правого
 	        	arr[iMerged] = right[iSecond];
+	        	//двигаемся в правом на 1
 	            iSecond++;
 	        }
+	        //в любом случае увеличиваем результирующий массив
 	        iMerged++;
 	    }
 	    
+	    //оставшиеся элементы - больше последнего (максимального) элемента одного из массивов. Докопируем оставшиеся элементы.
 	    System.arraycopy(left, iFirst, arr, iMerged, left.length - iFirst);
 	    System.arraycopy(right, iSecond, arr, iMerged, right.length - iSecond);
 	} //merge
@@ -253,8 +259,62 @@ class Sorts {
 	} //insertSort
 	
 	/*
+	 Сортировка вставками для однонаправленного связанного списка: 
+	 	* Сложность: N*N
+	 	* Доп. память: нет
+	 	* Общий смысл:
+	 		** движеся вправо по списку i
+	 		** если находим элемент меньше начала отсортированного списка, то делаем его началом отсортированного
+	 		** иначе идем с начала списка еще раз j, ищем место вставки в i, где текущий элемент i меньше следующего, но больше текущего - меняем элементы местами
+	 		** дошли до конца j , то это новый конец i
+	 */
+	public static Node InsertSortList(Node root) {
+		root = root.root;
+		if(root == null) return root;
+		
+		//новый отсортированный список
+		Node sort_node = new Node(root.val);
+		
+		Node pointer = root.next;
+		
+		//идем вперед
+		while(pointer != null) {
+			Node inner_node = sort_node;
+			Node next = pointer.next;
+			
+			//если элемент меньше текущего начала отсортированном, то это наше новое начало
+			if(pointer.val <= sort_node.val) {
+				Node swp = sort_node;
+				sort_node = pointer;
+				sort_node.next = swp;
+			} else {
+				//иначе идем с начала списка еще раз
+				while(inner_node.next != null) {
+					//находим место вставки
+					if (pointer.val > inner_node.val && pointer.val <= inner_node.next.val) {
+						Node oldNext = inner_node.next;
+						inner_node.next = pointer;
+						pointer.next = oldNext;
+					}					
+					inner_node = inner_node.next;
+				}
+				
+				//дошли доконца - это наш новый конец
+				if (inner_node.next == null && pointer.val > inner_node.val) {
+					inner_node.next = pointer;
+					pointer.next = null;
+				}
+			}
+			
+			pointer = next;
+		}
+		
+		return sort_node;
+	} //InsertSortList
+	
+	/*
 	 Быстрая сортировка: 
-	 	* Сложность: N*Log(N)
+	 	* Сложность: N*Log2(N)
 	 	* Доп. память: нет
 	 	* Минусы:
 	 	* - Сильно зависит от выбора средней точки, при неправильно выборе (крайний элемент) сложность = N*N
@@ -302,12 +362,16 @@ class Sorts {
 	 Поразрядная сортировка: 
 	 	* Сложность: N*K
 	 		** где K число разрядов в сортиуемом элементе
-	 	* Доп. память: дополнительный массив = входному
+	 	* Доп. память: O(N)
 	 	* Минусы/Плюсы:
-	 	* -/+ быстрей, если k < log(n) и дольше в других случаях
-	 	* - разный размер хэш массива для разных типов данных
+	 		* -/+ быстрей, если k < log2(n) и дольше в других случаях
+	 		* - разный размер хэш массива для разных типов данных
+	 	* Особенность:
+	 		* Порядок сортировки зависит от направления: для цифр нужно справа-навлево, для строк: слева-направо
 	 	* Общий смысл:
-	 		** 
+	 		* Идем по рязрядам числа справа-налево и помещаем элементы в массив по значению текущего разряда
+	 			** т.е. элементы сначала упорядычиваются по последнему элементу
+	 			** потом переупорядычиваваются по второму и т.д. пока полностью не упорядочим массив
 	 */
 	public static <T extends Integer> void radixSortUInt(T[] arr) {
 		
@@ -342,6 +406,7 @@ class Sorts {
 			    }
 		    }
 		    
+		    //разворачиваем двухуровневый массив обратно в последовательный
 		    int a = 0;
 		    for (int b = 0; b < RADIX; b++) {
 		    	for (int i = 0; i < bucket[b].size(); i++) {
@@ -349,25 +414,42 @@ class Sorts {
 		    	}
 		    	bucket[b].clear();
 		    }
+		    //переходим к следующему разряду
 		    placement *= RADIX;
 		}
 		  
 	} //radixSortUInt
 	
+	/*
+	 Бинарный посик: 
+	 	* Сложность: log2(N)
+	 	* Доп. память: нет
+	 	* Общий смысл:
+	 		* Идем по рязрядам числа справа-налево и помещаем элементы в массив по значению текущего разряда
+	 			** т.е. элементы сначала упорядычиваются по последнему элементу
+	 			** потом переупорядычиваваются по второму и т.д. пока полностью не упорядочим массив
+	 */
     public static <T extends Comparable<T>> int binary_search(T[] arr, T val) {
         int lo = 0;
         int hi = arr.length - 1;
+        //пока левая и правая границы не встретятся
         while (lo <= hi) {
+        	//находим середину
             int mid = lo + (hi - lo) / 2;
             
+            //если элемент меньше середины
             if (val.compareTo(arr[mid]) < 0) {
+            	//то верхняя граница - 1 = середина
             	hi = mid - 1;
             } else if (val.compareTo(arr[mid]) > 0) {
+            	//если больше, то нижняя граница = середина + 1
             	lo = mid + 1;
             } else {
+            	//иначе нашли
             	return mid;
             }
         }
+        //если границы встретились, то элемент не найден
         return -1;
     } //binary_search
 }
@@ -413,7 +495,7 @@ public class OracleSort extends MergeSort {
 		//Arrays.sort(arr);
 		//Sorts.bubleSort(arr);
 		//Sorts.insertSort(arr);
-		//InsertSortList.sort( new Node(arr) ).print();
+		Sorts.InsertSortList( new Node(arr) ).print();
 		
 		//(new Tree(arr)).traverse();
 		
@@ -423,7 +505,7 @@ public class OracleSort extends MergeSort {
 		
 		//Sorts.radixSortUInt(arr);
 		
-		new OracleSort(5, 2).sort(arr);
+		//new OracleSort(5, 2).sort(arr);
 		
 		System.out.println(Arrays.toString(arr));
 		
