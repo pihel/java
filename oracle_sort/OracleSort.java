ff5e2fcd8340cc840ed1dad9f9e0fcd5c1bc2b16
pihel <skahinpihel@gmail.com>
2016-10-16 21:40:52 +0300

    code format

--- oracle_sort/OracleSort.java
+++ oracle_sort/OracleSort.java
@@ -499,13 +499,12 @@ public class OracleSort extends MergeSort {
 
 	public static void main(String[] args) {
 		
-		
 		JUnitCore runner = new JUnitCore();
-        Result result = runner.run(OracleSortTest.class);
-        System.out.println("run tests: " + result.getRunCount());
-        System.out.println("failed tests: " + result.getFailureCount());
-        System.out.println("ignored tests: " + result.getIgnoreCount());
-        System.out.println("success: " + result.wasSuccessful());
+		Result result = runner.run(OracleSortTest.class);
+		System.out.println("run tests: " + result.getRunCount());
+		System.out.println("failed tests: " + result.getFailureCount());
+		System.out.println("ignored tests: " + result.getIgnoreCount());
+		System.out.println("success: " + result.wasSuccessful());
 		
 		/*Integer arr[] = {2, 6, 3, 5, 1, 7, 8, 0, 27, 17, 99, 13, 1, 7};
 		
