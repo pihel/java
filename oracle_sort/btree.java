41b08893a8e783f9df9dec00e7b4bd845ce169cc
pihel <skahinpihel@gmail.com>
2016-10-30 19:47:39 +0300

    btree

---
+++ oracle_sort/btree.java
@@ -0,0 +1,12 @@
+package oracle_sort;
+
+public class btree {
+
+	public static void main(String[] args) {
+		//http://www.juliandyke.com/Presentations/Presentations.html
+		//http://www.sql.ru/forum/661061/indeksy-b-tree-posmotret-cherez-dump-bloka-pravednik-au?mid=7139186#7139186
+		System.out.println("test");
+
+	}
+
+}
