7d9d16931fd2b1d7da23eac8d1ee4d82c5c44021
pihel <skahinpihel@gmail.com>
2017-02-08 21:15:56 +0300

    java fizzbuzz fix

--- algorithms/PrimeNumbers.java
+++ algorithms/PrimeNumbers.java
@@ -117,7 +117,7 @@ public class PrimeNumbers {
        
        public static void fizzbuzz() {
     	   for(int i = 1; i<= 100; i++) {
-    		   if(i%3 == 0 && i%5 == 0) System.out.print("FizzBuzz ");
+    		   if(i%15 == 0) System.out.print("FizzBuzz ");
     		   else if(i%3 == 0) System.out.print("Fizz ");
     		   else if(i%5 == 0) System.out.print("Buzz ");
     		   else System.out.print(i + " ");
