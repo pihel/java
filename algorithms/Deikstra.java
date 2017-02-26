a8aa8fe328b53970ff781ec57d19387e05220c17
pihel <skahinpihel@gmail.com>
2017-02-26 18:13:03 +0300

    java deikstra

--- algorithms/Deikstra.java
+++ algorithms/Deikstra.java
@@ -16,69 +16,133 @@ public class Deikstra {
 			System.out.println( " -> " + to_point + " (" + weight + ")");
 		} //dump
 	} //Rebro
+	
+	class Distance {
+		Double dist;
+		boolean processed;
+		
+		public Distance() {
+			dist = Double.MAX_VALUE;
+			processed = false;
+		} //Distance
+	} //Distance
 
 	//точка откуда, точка куда, вес ребра
 	//graph[откуда][].куда (вес)
-	HashMap<Integer, ArrayList<Rebro>> graph;
+	ArrayList<ArrayList<Rebro>> graph;
+	
+	//расстояние до рассчитываемой точки
+	Distance distance[];
 
 	public Deikstra() {
-		graph = new HashMap<Integer, ArrayList<Rebro>>();
+		graph = new ArrayList<ArrayList<Rebro>>();
 	} // Deikstra
 	
 	void add(Integer from_point, Integer to_point, Double weight) {
-		if(!graph.containsKey(from_point)) {
+		if( graph.size() <= from_point ) {
 			ArrayList<Rebro> rbr = new ArrayList<Rebro>();
-			graph.put(from_point, rbr);
-			graph.get(from_point).add(new Rebro(from_point, 0.0));
+			graph.add(from_point, rbr);
 		}
 		graph.get(from_point).add(new Rebro(to_point, weight));
 	} //add
 	
 	void getShortestWay(Integer from_point, Integer to_point) {
-		Integer pnt = from_point;
+		//добавим последнюю точку 1 раз
+		if(graph.get(graph.size()-1).size() > 0) {
+			graph.add(new ArrayList<Rebro>());
+		}
 		
-		//TODO
-		/*while(pnt != to_point) {
-			System.out.print(pnt);
-			graph.get(pnt).get(index)
-		}*/
+		distance = new Distance[graph.size()];
+		for(int i = 0; i < distance.length; i++) {
+			distance[i] = new Distance(); 
+		}
+		distance[from_point].dist = 0.0;
+		
+		Integer point = from_point;
+		while( point >= 0 ) {
+			
+			//get min point
+			point = -1;
+			Double min_distance = Double.MAX_VALUE;
+			for(int p = 0; p < distance.length; p++) {
+				if(!distance[p].processed && distance[p].dist < min_distance) {
+					min_distance = distance[p].dist;
+					point = p;
+				}
+			}
+			if(point < 0) continue;
+			
+			//calc min dist
+			ArrayList<Rebro> rbrs = graph.get(point);
+			for(int r = 0; r < rbrs.size(); r++) {
+				if(distance[rbrs.get(r).to_point].dist > ( distance[point].dist + rbrs.get(r).weight ) ) {
+					distance[rbrs.get(r).to_point].dist =  distance[point].dist + rbrs.get(r).weight;
+				}
+			}
+			
+			//set min point processed
+			distance[point].processed = true;
+		}
+		
+		//print
+		point = from_point;
+		Integer min_point = point;
+		
+		do {
+			point = min_point;
+			
+			System.out.print(point + " (" + distance[point].dist + ") -> ");
+			
+			Double min_dist = Double.MAX_VALUE;
+			
+			for(int r = 0; r < graph.get(point).size(); r++ ) {
+				if( distance[ graph.get(point).get(r).to_point ].dist  < min_dist ) {
+					min_point = graph.get(point).get(r).to_point;
+					min_dist = distance[min_point].dist;
+				} //if
+			} //for	
+		} while(point < to_point && point != min_point); //while
+		System.out.println("\r\n");
+		
 	} //getShortestWay
 	
 	void dump() {
-		for(Map.Entry<Integer, ArrayList<Rebro>> entry : graph.entrySet()) {
-			Integer from_point = entry.getKey();
-			ArrayList<Rebro> rbr_arr = entry.getValue();
+		System.out.println("-------------");
+		for(int p = 0; p < graph.size(); p++) {
 			
-			System.out.println(from_point);
+			if(distance.length > 0) {
+				System.out.println(p + " (" + distance[p].dist + ")");
+			} else {
+				System.out.println(p);
+			}
 			
-			for (Rebro rbr: rbr_arr) {
-				if(rbr.to_point != from_point) {
-					rbr.dump();
-				}
+			for (Rebro rbr: graph.get(p)) {
+				rbr.dump();
 			}			
 			
 			System.out.println(" ");
 		} //for
+		System.out.println("-------------");
 	} //dump
 
 	public static void main(String[] args) {
 		Deikstra d = new Deikstra();
-		d.add(1, 2, 7.0);
-		d.add(1, 3, 12.0);
+		d.add(0, 1, 7.0);
+		d.add(0, 2, 12.0);
 		
-		d.add(2, 3, 4.0);
-		d.add(2, 4, 15.0);
+		d.add(1, 2, 4.0);
+		d.add(1, 3, 15.0);
 		
-		d.add(3, 5, 5.0);
+		d.add(2, 4, 5.0);
 		
-		d.add(4, 6, 3.0);
+		d.add(3, 5, 3.0);
 		
-		d.add(5, 6, 11.0);
-		d.add(5, 4, 1.0);
+		d.add(4, 5, 11.0);
+		d.add(4, 3, 1.0);
 		
-		//d.dump();
+		d.getShortestWay(0, 5);
 		
-		d.getShortestWay(1, 6);
+		d.dump();
 	}
 
 }
